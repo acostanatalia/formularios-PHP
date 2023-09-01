1dcc36b9915992a4f7c39e1628bbf7ed4f51824c
Natalia Acosta <acostanatali89@gmail.com>
2023-09-01 00:57:29 +0000

    cambio1

---
+++ clase 31 ago/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PHP FORMULARIO </title>
+</head>
+
+<body>
+    <h1>FORMULARIO PHP</h1>
+     <form  method="post" action="">
+
+    Email Address:
+
+<br>
+
+    <input type="text" name="email" value="<?php echo $email; ?>">
+
+     </form>
+
+
+    
+</body>
+</html>
